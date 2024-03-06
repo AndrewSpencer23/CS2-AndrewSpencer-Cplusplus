f0cbd1409e40c5ff1f0d92a177e80cc3b8de4fe7
AndrewSpencer23 <alspencer@mavs.coloradomesa.edu>
2024-03-06 11:18:21 -0800

    HW1 Commit

---
+++ Homework/HW1/HW1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "HW1.h"
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    return 0;
+}
---
+++ Homework/HW1/HW1.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <string>
+using namespace std;
+
+namespace movies {
+
+    class Movie { 
+        private: // declaring variables in class string _id;
+            string _title; 
+            int _year; 
+            string _genre; 
+            float _rating; 
+            string _director;
+
+            public:
+
+        }; 
+
+    class Database {
+        private:
+
+        public:
+
+    };
+}
