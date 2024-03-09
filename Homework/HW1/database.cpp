afadd3e73b781a5a1083696b9f973768e10208d3
alspencer <alspencer@mavs.coloradomesa.edu>
2024-03-09 15:56:18 -0800

    Homework progress

--- Homework/HW1/database.cpp
+++ Homework/HW1/database.cpp
@@ -1,1 +1,7 @@
 #include "database.h"
+
+void databases::Database::initMovies() {
+    for(int i = 0; i < 100; i++) {
+        _movieList[i] = nullptr;
+    }
+}
--- Homework/HW1/database.h
+++ Homework/HW1/database.h
@@ -1,11 +1,21 @@
 #pragma once
 
-namespace movies {
+#include "movie.h"
+
+#include <string>
+
+using namespace std;
+
+namespace databases {
     class Database { 
         private: // Declaring variables in class
-
+            int _db_id;
+            string _dbname;
+            movies::Movie* _movieList[100];
+            int _numMovies = 0;
         public:
-        Database(); // Constructor
-        ~Database(); // Destructor
+            void initMovies();
+            Database(); // Constructor
+            ~Database(); // Destructor
     };
 }
