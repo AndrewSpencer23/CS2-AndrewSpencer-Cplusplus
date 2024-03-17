1ffa930fa8789fd49c4a992e988bf1f90031964a
AndrewSpencer23 <alspencer@mavs.coloradomesa.edu>
2024-03-17 16:52:32 -0700

    HW1 Commit

--- Homework/HW1/main.cpp
+++ Homework/HW1/main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <fstream>
 
 #include "database.h"
 
@@ -7,5 +10,24 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
+    if(argc >= 2 && (string)argv[1] == "test")
+    {
+        return 0;
+    }
+
+    int numMovies; // Will update later, this will be how many lines are read in from the movies file.
+
+    movies::Movie movieList[numMovies]; // Creating instance of a movie class using an array of movies.
+
+    databases::Database* movieDatabase; // Creating an instance of a movie database as a pointer.
+
+    ifstream fin;
+    fin.open("movies.csv");
+
+    fin.close();
     return 0;
+}
+
+void tests() {
+
 }
