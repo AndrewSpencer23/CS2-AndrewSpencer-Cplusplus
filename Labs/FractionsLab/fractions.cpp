490c916874960f4f35b74f7eae60d4b504bb9d0a
AndrewSpencer23 <alspencer@mavs.coloradomesa.edu>
2024-04-05 16:30:25 -0700

    Fractions lab commit

--- Labs/FractionsLab/fractions.cpp
+++ Labs/FractionsLab/fractions.cpp
@@ -2,26 +2,54 @@
 
 fractions::Fraction::Fraction(int numerator, int denominator)
 {
+    _numerator = numerator;
+    _denominator = denominator;
 }
 
 fractions::Fraction fractions::Fraction::operator+(fractions::Fraction const &frac)
 {
-    return Fraction(0, 1);
+    int commonDenom = 0;
+    commonDenom = _denominator * frac._denominator;
+
+    int num1, num2;
+    num1 = _numerator * frac._denominator;
+    num2 = frac._numerator * _denominator;
+
+    _numerator = num1 + num2;
+    _denominator = commonDenom;
+
+    return Fraction(_numerator, _denominator);
 }
 
 fractions::Fraction fractions::Fraction::operator-(fractions::Fraction const &frac)
 {
-    return Fraction(0, 1);
+    int commonDenom = 0;
+    commonDenom = _denominator * frac._denominator;
+
+    int num1, num2;
+    num1 = _numerator * frac._denominator;
+    num2 = frac._numerator * _denominator;
+
+    _numerator = num1 - num2;
+    _denominator = commonDenom;
+
+    return Fraction(_numerator, _denominator);
 }
 
 fractions::Fraction fractions::Fraction::operator*(Fraction const &frac)
 {
-    return Fraction(0, 1);
+    _numerator = _numerator * frac._numerator;
+    _denominator = _denominator * frac._denominator;
+
+    return Fraction(_numerator, _denominator);
 }
 
 fractions::Fraction fractions::Fraction::operator/(Fraction const &frac)
 {
-    return Fraction(0, 1);
+    _numerator = _numerator * frac._denominator;
+    _denominator = _denominator * frac._numerator;
+
+    return Fraction(_numerator, _denominator);
 }
 
 fractions::Fraction fractions::Fraction::simplify(Fraction frac)
