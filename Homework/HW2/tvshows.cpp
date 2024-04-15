f4191d929b5c49a4d34188c19076e36910f15c1b
AndrewSpencer23 <alspencer@mavs.coloradomesa.edu>
2024-04-15 15:25:04 -0700

    HW2 commit

--- Homework/HW2/tvshows.cpp
+++
@@ -1,1 +0,0 @@
-#include "tvshows.h"
--- Homework/HW2/tvshows.h
+++ Homework/HW2/tvshows.h
@@ -1,1 +1,26 @@
 #pragma once
+#include "tvshows.h"
+#include "media.h"
+
+#include <string>
+
+namespace tvshows {
+    class tvShow : media::Media {
+        private:
+            string _tvGenre;
+            float _tvRating;
+            int _numEpisodes;
+    
+        public:
+            tvShow();
+            ~tvShow();
+
+            void setTvGenre(string);
+            void setTvRating(float);
+            void setNumEpisodes(int);
+            
+            string getTvGenre();
+            float getTvRating();
+            int getNumEpisodes();
+    };
+}
