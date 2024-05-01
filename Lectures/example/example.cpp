eef8a51db470a35a47267836edd27e98e6c98d2e
AndrewSpencer23 <alspencer@mavs.coloradomesa.edu>
2024-04-30 23:03:05 -0700

    commit

---
+++ Lectures/example/example.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    int array[5];
+    array[0] = 10, array[1] = 20, array[2] = 30, array[3] = 40, array[4] = 50;
+    cout << array[2] << endl;
+    return 0;
+}                                                                                                                                                                                 
