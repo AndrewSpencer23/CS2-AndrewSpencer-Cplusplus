e24004410820af624f1b2a2b347506d5ffb247a6
AndrewSpencer23 <alspencer@mavs.coloradomesa.edu>
2024-05-09 21:47:24 -0700

    Lab 5 commit

--- Labs/BST/tree.h
+++ Labs/BST/tree.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "node.h"
+#include <iostream>
 
 using namespace std;
 
@@ -8,30 +9,32 @@ class BST
 {
 private:
     Node<T1> *_root;
+
     Node<T1> *insertNode(Node<T1> *, T1);
     void inOrderPrint(Node<T1> *);
     Node<T1> *searchData(Node<T1> *, T1);
     Node<T1> *removeData(Node<T1> *, T1);
     Node<T1> *minVal(Node<T1> *);
-    void destroyTree(Node<T1>* root);
+    void destroyTree(Node<T1>*);
 
 public:
+    BST();
+    ~BST();
     void insert(T1);
     void inOrder();
     bool search(T1);
     void remove(T1);
-    ~BST();
 };
 
-// Destructor, should call destroyTree, passing _root to that method
+template <class T1>
+BST<T1>::BST() : _root(nullptr) {}
+
 template <class T1>
 BST<T1>::~BST()
 {
     destroyTree(_root);
 }
 
-// This should recursively walk the tree and delete a node if both leafs are null. You can return from a void function if you just have the return statement.
-// This is useful if root == nullptr...
 template <class T1>
 void BST<T1>::destroyTree(Node<T1>* root)
 {
@@ -43,18 +46,67 @@ void BST<T1>::destroyTree(Node<T1>* root)
     delete root;
 }
 
-// Public method that takes in some data and passes that into the removeData method. Don't forget to update _root as you may end up deleting the original root.
 template <class T1>
-void BST<T1>::remove(T1 data)
+Node<T1> *BST<T1>::insertNode(Node<T1> *root, T1 data)
 {
-    _root = removeData(_root, data);
+    if(root == nullptr) {
+        Node<T1>* addedNode = new Node<T1>();
+        addedNode->setData(data);
+        return addedNode;
+    }
+    if(data < root->getData()) {
+        root->setLeft(insertNode(root->getLeft(), data));
+    }
+    else if(data > root->getData()) {
+        root->setRight(insertNode(root->getRight(), data));
+    }
+    return root;
 }
 
-// Private method to recursively walk the tree until the data is found.
-// Then using the process built in class, check if there is only one leaf, if so then move that leaf into the root position.
-// If there are two leafs, find the smallest value in the right subtree (or the largest value in the left subtree but you would have to rename minVal method) and then replace
-// the current node with that.
-// Don't forget to clean up your memory!
+template <class T1>
+void BST<T1>::insert(T1 data)
+{
+    _root = insertNode(_root, data);
+}
+
+template <class T1>
+void BST<T1>::inOrderPrint(Node<T1> *root)
+{
+    if(root == nullptr) {
+        return;
+    }
+    inOrderPrint(root->getLeft());
+    cout << root->getData() << " ";
+    inOrderPrint(root->getRight());
+}
+
+template <class T1>
+void BST<T1>::inOrder()
+{
+    inOrderPrint(_root);
+    cout << endl;
+}
+
+template <class T1>
+Node<T1> *BST<T1>::searchData(Node<T1> *root, T1 data)
+{
+    if(root == nullptr || root->getData() == data) {
+        return root;
+    }
+    if(data < root->getData()) {
+        return searchData(root->getLeft(), data);
+    }
+    return searchData(root->getRight(), data);
+}
+
+template <class T1>
+bool BST<T1>::search(T1 data)
+{
+    Node<T1>* searchNode;
+    searchNode = searchData(_root, data);
+    return searchNode != nullptr;
+}
+
 template <class T1>
 Node<T1> *BST<T1>::removeData(Node<T1> *root, T1 data)
 {
@@ -81,12 +133,10 @@ Node<T1> *BST<T1>::removeData(Node<T1> *root, T1 data)
         Node<T1>* tempNode = minVal(root->getRight());
         root->setData(tempNode->getData());
         root->setRight(removeData(root->getRight(), tempNode->getData()));
-        delete tempNode;
     }
     return root;
 }
 
-// Given a node, find the smallest value in that subtree. Return that node
 template <class T1>
 Node<T1> *BST<T1>::minVal(Node<T1> *root)
 {
@@ -97,78 +147,8 @@ Node<T1> *BST<T1>::minVal(Node<T1> *root)
     return currentNode;
 }
 
-// Given data and a node, recursively walk the tree to find that node if it exists.
 template <class T1>
-Node<T1> *BST<T1>::searchData(Node<T1> *root, T1 data)
+void BST<T1>::remove(T1 data)
 {
-    if(root == nullptr || root->getData() == data) {
-        return root;
-    }
-    if(data < root->getData()) {
-        return searchData(root->getLeft(), data);
-    }
-    return searchData(root->getRight(), data);
-}
-
-// Wrapper function for searchData. Pass data, root into searchData and return true if data found, return false if data not found.
-template <class T1>
-bool BST<T1>::search(T1 data)
-{
-    Node<T1>* searchNode;
-    searchNode = searchData(_root, data);
-    if(searchNode == nullptr) {
-        return false;
-    }
-    return true;
-}
-
-// Given a node, recursively walk the tree to print out the inOrder format. That's left->root->right.
-// Make sure you cout with a space separating each value as I based my tests on that!
-// For example, you would end up cout (do not add the "): "1 2 3 4 5 "
-// Notice that there is an extra space at the end.
-template <class T1>
-void BST<T1>::inOrderPrint(Node<T1> *root)
-{
-    if(_root == nullptr) {
-        return;
-    }
-    inOrderPrint(root->getLeft());
-    cout << root->getData() << " ";
-    inOrderPrint(root->getRight());
-}
-
-// Wrapper for inOrderPrint
-template <class T1>
-void BST<T1>::inOrder()
-{
-    inOrderPrint(_root);
-    cout << endl;
-}
-
-// Give some data and a node, recursively walk the tree until you get to a nullptr and store the value there.
-// You should ensure that if data < the current node's data, you walk the left. If data > current node's data, walk right.
-// You should check if the value already exists and say so (it's just adding an else with a cout).
-template <class T1>
-Node<T1> *BST<T1>::insertNode(Node<T1> *root, T1 data)
-{
-    if(root == nullptr) {
-        Node<T1>* addedNode = new Node<T1>();
-        addedNode->setData(data);
-        return addedNode;
-    }
-    if(data < root->getData()) {
-        root->setLeft(insertNode(root->getLeft(), data));
-
-    }
-    else if(data > root->getData()) {
-        root->setRight(insertNode(root->getRight(), data));
-    }
-    return root;
-}
-
-// Wrapper for insertNode. Take in data to pass that and _root to insertNode. Ensure you update _root since if the tree is empty, that would be the new _root.
-template <class T1>
-void BST<T1>::insert(T1 data)
-{
-    _root = insertNode(_root, data);
+    _root = removeData(_root, data);
 }
